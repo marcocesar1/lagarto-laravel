0f87b2a842abcacd8af3f626b16c8568b1d5ac01
Marco <mark_o002@hotmail.com>
2024-08-07 20:05:09 -0600

    md file

--- tests/Unit/ExamenTest.php
+++ tests/Unit/ExamenTest.php
@@ -5,19 +5,14 @@ namespace Tests\Unit;
 use App\Services\Examen;
 use PHPUnit\Framework\TestCase;
 
-class ExampleTest extends TestCase
+class ExamenTest extends TestCase
 {
-    /**
-     * A basic test example.
-     */
     public function test_lagarto_with_specific_parameters(): void
     {
         $examen = new Examen();
 
         $response = $examen->execute(
-            //0,0,0,0
-            6, 3, 1, 10 
-            //3, 3, 0, 10 
+            6, 3, 1, 10
         );
 
         $this->assertEquals('success on day 3', $response['hora']);
